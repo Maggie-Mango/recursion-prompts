31be4981147794ba6fdf0df8be7637d1d2c7a95f
Maggie Saldivia <Maggiesaldivia@gmail.com>
2021-05-25 23:59:16 -0700

    Pass all but one test in #6

--- src/recursion.js
+++ src/recursion.js
@@ -83,6 +83,24 @@ var sumBelow = function(n) {
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  if (x === y) {
+    return [];
+  }
+  if (y - x === 1) {
+    return [];
+  }
+
+  //the === 2 part is specifically for getting the right amount of Nums for our array
+  if (y - x === 2) {
+    return [x + 1];
+  } else {
+    // 9, 8, 7, 6, 5,
+    var result = range(x, y - 1);
+    //push 4, 5, 6, 7, 8
+    result.push(y - 1);
+    return result;
+  }
+
 };
 
 // 7. Compute the exponent of a number.
