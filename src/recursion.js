c4e49d46c28fe12dd51feaa71235d5e2953c42da
Maggie Saldivia <Maggiesaldivia@gmail.com>
2021-06-02 13:13:16 -0700

    Pass test #10 - Palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -168,7 +168,24 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  //base case
+  string = string.toLowerCase();
+  if (string.length <= 1) {
+    return true;
+  }
+  let firstLetter = string[0];
+  let lastLetter = string[string.length - 1];
+
+  if (firstLetter === lastLetter) {
+    var word = string.substring(1, string.length - 1);
+    //console.log(word);
+    return palindrome(word);
+  } else {
+    return false;
+  }
+
 };
+
 
 // 11. Write a function that returns the remainder of x divided by y without using the
 // modulo (%) operator.
