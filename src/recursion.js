f918bd56aa15bcdf0138d1a6340f154574978592
Maggie Saldivia <Maggiesaldivia@gmail.com>
2021-07-07 18:58:00 -0700

    pass Compare Strings tests

--- src/recursion.js
+++ src/recursion.js
@@ -281,7 +281,18 @@ var gcd = function(x, y) {
 // compareStr('house', 'houses') // false
 // compareStr('tomato', 'tomato') // true
 var compareStr = function(str1, str2) {
-};
+
+  if (str1.length === str2.length) {
+     return true;
+   }
+
+
+   if (str1[0] === str2[0]) {
+     return compareStr(str1.slice(1), str2.slice(1));
+   }
+   return false;
+ };
+
 
 // 16. Write a function that accepts a string and creates an array where each letter
 // occupies an index of the array.
